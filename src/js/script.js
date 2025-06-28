c9d60890946536c3e7345fceb30d59eb0a2873f4
CarlosHenrique <henriquevilas764@gmail.com>
2025-06-27 23:21:33 -0300

    Modificando o código fonte, trazendo nova estilização e novo htmtl e colocando funcionalidades completamentes novas no JavaScript - LocalStorage, POO e etc...

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,45 @@
 // puxando os elementos do html
-const inputTask = document.querySelector('#text-task')
+const titleInput = document.querySelector('#title')
+const descriptionInput = document.querySelector('#description')
+const select = document.querySelector('#select')
 const btnTask = document.querySelector('#btn-create')
-const card = document.querySelectorAll('.card')
+const cardInitial = document.querySelector('.card-initial')
+const cardMid = document.querySelector('.card-mid')
+const cardEnd = document.querySelector('.card-end')
 
+class Task {
+    constructor(title, description, priority) {
+        this.title = title
+        this.description = description
+        this.priority = priority
+    }
+}
 
 function sendValue() {
-    const valueTask = inputTask.value
-    const acessCard = document.querySelector('.card.active')
-    // Dom
-    const templateString = `
-    <div class="task-card">
-    <span class="text-task"> ${valueTask} </span>
-    <div class="button-space">
-    <button class="check"><img src="./src/img/checkV.png" alt="check"></button>
-    <button class="bad"><img src="./src/img/cancelar.png" alt="no-complete"></button> </div>
-    </div>
-    `
-    const parser = new DOMParser()
-    const htmlTemplate = parser.parseFromString(templateString, "text/html")
-    const taskCard = htmlTemplate.querySelector('.task-card')
-    // const button = document.createElement('button')
-    // button.className = ''
-    // button.setAttribute('src', './delete.png')
+    const valueTitle = titleInput.value
+    const valueDescription = descriptionInput.value
+    const valueSelect = select.value
+    const newTask = new Task(valueTitle, valueDescription, valueSelect)
+    const getTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    getTasks.push(newTask)
+    localStorage.setItem('tasks', JSON.stringify(getTasks))
 
-    // botões dos task-card
-    const buttonCheck = taskCard.querySelector('.check')
-    const buttonBad = taskCard.querySelector('.bad')
-    const textTask = taskCard.querySelector('.text-task')
-    buttonCheck.addEventListener('click', () => {
-        textTask.style.color = '#01e93b'
-    })
-    buttonBad.addEventListener('click', ()=>{
-        textTask.style.color = '#f10000'
-    })
-    acessCard.appendChild(taskCard);
 }
 // eventos
-card.forEach(cardClick => {
-    cardClick.addEventListener('click', () => {
-        cardClick.classList.add('active')
+function domTask() {
+    const getResult = JSON.parse(localStorage.getItem('tasks'))
+    getResult.forEach(taskObject => {
+        console.log(taskObject.title)
+    });
+}
 
-    })
-    cardClick.addEventListener('dblclick', () => {
-        cardClick.classList.remove('active')
-    })
-
-});
-
+function cleanInputs() {
+    titleInput.value = ''
+    descriptionInput.value = ''
+    select.value = ''
+}
 btnTask.addEventListener('click', (e) => {
     e.preventDefault()
     sendValue()
+    cleanInputs()
 })
